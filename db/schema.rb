929c6b6d23f7c03c8da82ea7fd92011fd10832f4
fede <fedetob@gmail.com>
2020-05-31 19:07:19 -0300

    Se agrega year a vehicles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200505220728) do
+ActiveRecord::Schema.define(version: 20200531212619) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -442,6 +442,7 @@ ActiveRecord::Schema.define(version: 20200505220728) do
     t.integer  "default_driver_id"
     t.string   "gps_numeric_ident",   limit: 255
     t.datetime "creation_date"
+    t.integer  "year"
   end
 
   add_index "vehicles", ["vehicle_brand_id"], name: "index_vehicles_on_vehicle_brand_id", using: :btree
